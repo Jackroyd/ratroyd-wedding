9fe2975f5577ec497aa9b1d672e632f7e65b5ed7
Josh Ackroyd <joshua.ackroyd@winnowsolutions.com>
2024-03-05 22:19:59 +0000

    changed wording and links

--- src/components/accommodation_accordion.js
+++ src/components/accommodation_accordion.js
@@ -11,8 +11,8 @@ const AccommodationAccordian = ({titleTextSize, mainTextSize}) => {
         </AccordionButton>
         <AccordionPanel>
           <Text fontFamily={'main_text'} fontSize={mainTextSize} padding={'0 0 1rem 2rem'}>
-          The manor house has lots of space if you want somewhere to crash. There are sofas and areas for makeshift beds. 
-          Remember to bring a sleeping bag and blow up bed if you need it! All the proper beds in the house have already been allocated to close friends and family.
+          Beds have already been allocated to close friends and family. However, the manor house has lots of space if you want somewhere to crash, with sofas and areas for makeshift beds. 
+          Remember to bring a sleeping bag and blow up bed if you need it! 
           </Text>
         </AccordionPanel>
       </AccordionItem>
@@ -24,7 +24,7 @@ const AccommodationAccordian = ({titleTextSize, mainTextSize}) => {
         <AccordionPanel>
           <Text fontFamily={'main_text'} fontSize={mainTextSize} padding={'0 0 1rem 2rem'}>
             For the outdoorsy ones, the estate has a large camping area for tents and camper vans.
-            You'll find great facilities with high quality showers, toilets and an area to get ready. 
+            You'll find <Link textDecoration={'underline'} textDecorationColor={'yellowish'} href='https://www.ashbarton.com/amenities/camping' target='_blank'>great facilities</Link> with high quality showers, toilets and an area to get ready. 
           </Text>
         </AccordionPanel>
       </AccordionItem>
@@ -35,7 +35,7 @@ const AccommodationAccordian = ({titleTextSize, mainTextSize}) => {
         </AccordionButton>
         <AccordionPanel>
           <Text fontFamily={'main_text'} fontSize={mainTextSize} padding={'0 0 1rem 2rem'}>
-            For a bit more comfort, you can <Link textDecoration={'underline'} textDecorationColor={'yellowish'} href='https://www.nativebelltents.co.uk/prices-1'>hire a yurt</Link> from Fri-Mon (3 nights) on the estate:
+            For a bit more comfort, you can <Link textDecoration={'underline'} textDecorationColor={'yellowish'} href='https://www.nativebelltents.co.uk/prices-1' target='_blank'>hire a yurt</Link> from Fri-Mon (3 nights) on the estate:
           </Text>
           <UnorderedList fontFamily={'main_text'} fontSize={mainTextSize} padding={'0 0 1rem 2rem'}>
             <ListItem>2-3 people: £200</ListItem>
@@ -44,7 +44,7 @@ const AccommodationAccordian = ({titleTextSize, mainTextSize}) => {
           </UnorderedList>
           <Text fontFamily={'main_text'} fontSize={mainTextSize} padding={'0 0 1rem 2rem'}>
             Please indicate in the RSVP form if you're interested and we'll book these for you.
-            You'll find great facilities with high quality showers, toilets and an area to get ready.         
+            You'll find <Link textDecoration={'underline'} textDecorationColor={'yellowish'} href='https://www.ashbarton.com/amenities/camping' target='_blank'>great facilities</Link> with high quality showers, toilets and an area to get ready.         
           </Text>
         </AccordionPanel>
       </AccordionItem>
@@ -56,7 +56,7 @@ const AccommodationAccordian = ({titleTextSize, mainTextSize}) => {
         <AccordionPanel>
           <Text fontFamily={'main_text'} fontSize={mainTextSize} padding={'0 0 1rem 2rem'}>
             There are plenty of locations you can book around the area.
-            We've put together a list of places, which includes recommendations by the venue, as well as highly-rated AirBnBs.
+            We've put together a  <Link textDecoration={'underline'} textDecorationColor={'yellowish'} href='https://docs.google.com/spreadsheets/d/1F506gMXRPlAftezvTrqdCS_J-S7ghcoJ2nIAtFaV2RE/edit#gid=0' target='_blank'>list of places</Link>, which includes recommendations by the venue, as well as highly-rated AirBnBs.
             <br/>
             <br/>
             Booked a place? Let us know so we can strike it from the list.          
